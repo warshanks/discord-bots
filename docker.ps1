a8cb0fc03865875292dccc5ddfbcc40f1a928a56
Ben Shankles <benshankles@gmail.com>
2023-05-01 07:43:26 -0500

    Create docker.ps1

---
+++ docker.ps1
@@ -0,0 +1,16 @@
+# Set the name of your Docker image
+$ImageName = "warshanks/discord-bots:3.11"
+
+# Build the Docker image
+docker build -t $ImageName -f dockerfile.3.11 .
+
+# Set the paths to the directories you want to mount
+$LocalDir1 = "C:\Users\Ben\Documents\GitHub\discord-bots\fastf1_cache"
+$LocalDir2 = "C:\Users\Ben\Documents\GitHub\discord-bots\src"
+
+# Set the paths where you want to mount the directories in the container
+$ContainerDir1 = "/fastf1_cache"
+$ContainerDir2 = "/app"
+
+# Run the Docker container with the two directory mounts in detached mode
+docker run -d -v ${LocalDir1}:${ContainerDir1} -v ${LocalDir2}:${ContainerDir2} ${ImageName}
