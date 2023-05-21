949c5bccfda0c59af4cb8cb903f57c030bc6a90c
Shanks <10147809+warshanks@users.noreply.github.com>
2023-05-20 22:36:10 -0500

    Linting

--- src/cogs/image_cog.py
+++ src/cogs/image_cog.py
@@ -1,3 +1,18 @@
+"""
+This module provides a Discord bot with the capability to generate images using both
+OpenAI's Dall-error_message model and Stability.AI's Stable Diffusion model.
+
+The bot uses discord.py's command extension to create commands that users can invoke in chat.
+
+The image generation models are accessible through the ImageCog class, which contains
+two commands:
+- 'dall-e': Generates an image from a text prompt using OpenAI's Dall-e model.
+- 'stable': Generates an image from a text prompt using Stability.AI's Stable Diffusion model.
+
+API keys and other configurations for OpenAI and Stability.AI are loaded from a separate
+config module.
+"""
+
 import base64
 import datetime
 import io
@@ -31,7 +46,13 @@ stability_api = client.StabilityInference(
 
 # noinspection PyShadowingNames
 class ImageCog(commands.Cog):
+    """A Cog (a collection of commands) for image generation."""
     def __init__(self, bot):
+        """Initialize the ImageCog with a bot instance.
+
+        Args:
+            bot (commands.Bot): The bot instance for this cog.
+        """
         self.bot = bot
 
     # Define a slash command that sends a prompt for image generation to OpenAI
@@ -39,6 +60,13 @@ class ImageCog(commands.Cog):
                       description="Generate an image from a prompt using "
                                   "OpenAI's Dall-e model")
     async def generate_image_dalle(self, ctx, img_prompt: str):
+        """Generate an image using OpenAI's Dall-error_message model.
+
+        Args:
+            ctx (commands.Context): The context in which the command was called.
+            img_prompt (str): The prompt from which to generate an image.
+        """
+
         # Defer the response to let the user know that the bot is working on the request
         await ctx.response.defer(thinking=True, ephemeral=False)
         # Print information about the user, guild and channel where the command was invoked
@@ -61,8 +89,8 @@ class ImageCog(commands.Cog):
                                             f'by KC & {ctx.user} '
                                             f'c. {datetime.datetime.now().year}',
                                     file=discord.File(fp="./images/dalle.png"))
-        except Exception as e:
-            await ctx.followup.send(e)
+        except Exception as error_message:
+            await ctx.followup.send(error_message)
 
     # Define a slash command that sends a prompt for image generation to Stability.AI
     @bot.tree.command(name='stable',
@@ -71,6 +99,18 @@ class ImageCog(commands.Cog):
     async def generate_image_stable(self, ctx, img_prompt: str, img_seed: int = None,
                                     img_steps: int = 30, cfg_mod: float = 8.0,
                                     img_width: int = 512, img_height: int = 512):
+        """Generate an image using Stability.AI's Stable Diffusion model.
+
+        Args:
+            ctx (commands.Context): The context in which the command was called.
+            img_prompt (str): The prompt from which to generate an image.
+            img_seed (int, optional): The seed for the image generation. Defaults to None.
+            img_steps (int, optional): The number of steps for the image generation. Defaults to 30.
+            cfg_mod (float, optional): The config scale for the image generation. Defaults to 8.0.
+            img_width (int, optional): The width of the generated image. Defaults to 512.
+            img_height (int, optional): The height of the generated image. Defaults to 512.
+        """
+
         # Defer the response while the bot processes the image generation request
         await ctx.response.defer(thinking=True, ephemeral=False)
 
@@ -112,5 +152,5 @@ class ImageCog(commands.Cog):
                                                                   filename=str(artifact.seed)
                                                                   + ".png"))
         # Catch any exceptions and send an ephemeral message with the error
-        except Exception as e:
-            await ctx.followup.send(e, ephemeral=True)
+        except Exception as error_message:
+            await ctx.followup.send(error_message)
